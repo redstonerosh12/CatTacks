ab5b3a80b2d0ec4213b30e3b483f71e09a624499
Roshan Emmanuel <rosh.notes@gmail.com>
2025-03-12 18:07:35 -0700

    added variable support python3 cacl.py opens as default using now but python3 calc.py HH:MM works using time specified

--- calc.py
+++ calc.py
@@ -1,14 +1,17 @@
 import data
 import datetime
+import sys
+import re
 
-def main():
+def main(now=datetime.datetime.now().time().replace(second=0, microsecond=0), isPreTime=False):
     print(f"supported places: {data.SupportedPlaces}")
     place = input("Place to go: ")
-    now = datetime.datetime.now().time().replace(second=0, microsecond=0)
     availableTime = False
     for bus in data.Buses:
         if place in bus.destinations:
             nextBus = next((t for t in bus.timings if t >= now))
+            if isPreTime:
+                print(f"Leaving at time {now},", end=" ")
             print(f"For bus {bus.name}, The next bus is at: {nextBus}")
             availableTime = True
 
@@ -16,6 +19,17 @@ def main():
         print("sorry there are no buses in the current scope of the project")
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        time = sys.argv[1]
+        pattern = r'^\d{1,2}:\d{1,2}$'  # Matches 1-2 digits, colon, then 2 digits
+        if re.match(pattern, time):
+            pretime = time.split(":")
+            pretimetime = data.t(int(pretime[0]), int(pretime[1]))
+            main(pretimetime, isPreTime=True)
+        else:
+            print("Error with pre defined time defintion defaulting to now")
+            main()
+    else:
+        main()
 
 
